645081c0a1b9cab19cc3cbbf866d69089278776d
xiangyisss <[xiangyisunn@gmail.com]>
2022-05-18 00:55:36 +0200

    first commit add counter store

---
+++ src/features/Counter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment, incrementByAmount } from './counterSlice';
+
+export const Counter = () => {
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+
+      <span>{count}</span>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(incrementByAmount(10))}>
+        Increment by amount
+      </button>
+    </div>
+  );
+};
